7acd7b3d9dd2b606b5ee6d81c5a92e5dc633b95b
Fabiola <fabiola.caso@unitn.it>
2020-02-01 13:26:47 +0100

    router-2 implementation

---
+++ router2.sh
@@ -0,0 +1,13 @@
+#libraries and functions
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y tcpdump --assume-yes
+sudo su
+sysctl -w net.ipv4.ip_forward=1
+#commands
+ip link set dev enp0s8 up
+ip link set dev enp0s9 up
+ip add add 192.168.3.1/22 dev enp0s8
+ip add add 192.168.4.2/22 dev enp0s9
+ip route add 192.168.1.0/24 via 192.168.4.1
+ip route add 192.168.2.0/24 via 192.168.4.1
