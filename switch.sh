f45a16d6e89172a0bef9dde75626b5aa60022f20
Fabiola <fabiola.caso@unitn.it>
2020-02-01 13:27:01 +0100

    switch implementation

--- switch.sh
+++ switch.sh
@@ -1,6 +1,15 @@
+#libraries and functions
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump
 apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-certificates curl software-properties-common
-
-# Startup commands for switch go here
+sudo su
+#commands
+ovs-vsctl add-br switch
+ovs-vsctl add-port switch enp0s8
+ovs-vsctl add-port switch enp0s9 tag=10
+ovs-vsctl add-port switch enp0s10 tag=20
+ip link set dev enp0s8 up
+ip link set dev enp0s9 up
+ip link set dev enp0s10 up
+ip link set dev ovs-system up
