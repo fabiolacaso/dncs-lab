1f22f81483349510e8882fbaf22286cfd2b4af0b
Fabiola <fabiola.caso@unitn.it>
2020-02-01 13:26:15 +0100

    host-b implementation

---
+++ hostb.sh
@@ -0,0 +1,10 @@
+#libraries and functions
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y tcpdump --assume-yes
+sudo su
+#commands
+ip addr add 192.168.2.2/24 dev enp0s8
+ip link set dev enp0s8 up
+ip route del default
+ip route add default via 192.168.2.1
